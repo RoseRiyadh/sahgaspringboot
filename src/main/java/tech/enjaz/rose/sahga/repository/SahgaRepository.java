8c7e1584096ba28c33de7fd99b864463a4389b14
RoseRiyadh <roseriyadh.p@gmail.com>
2020-09-08 23:33:30 +0300

    Ayyy yooo

--- src/main/java/tech/enjaz/rose/sahga/repository/SahgaRepository.java
+++ src/main/java/tech/enjaz/rose/sahga/repository/SahgaRepository.java
@@ -1,12 +1,14 @@
 package tech.enjaz.rose.sahga.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 import tech.enjaz.rose.sahga.entity.Sahga;
 
+@Repository
 public interface SahgaRepository extends CrudRepository<Sahga, Long> {
     List<Sahga> findAll();
 }
